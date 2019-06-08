f103d0ef6f1dbfa65bf9ff432e8732251b59271c
Andrew Paxson <apaxson@ilm.com>
2019-06-08 03:42:33 -0700

    Initial Commit

---
+++ MapKitGame/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  MapKitGame
+//
+//  Created by Andrew Paxson on 2019-06-08.
+//  Copyright © 2019 Andrew Paxson. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
